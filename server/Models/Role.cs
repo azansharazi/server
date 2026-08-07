0170fd1b160a1b00d60056b8dc6ddd363036ec06
azansharazi <azansharazi@gmail.com>
2026-08-07 05:59:32 +0500

    FluentApli configration done

---
+++ server/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace server.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+        //UserRoles
+
+        public ICollection<UserRole>? UserRoles { get; set; }
+    }
+}
